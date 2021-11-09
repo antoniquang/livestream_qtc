273fec04e7b4f26216921b06f6c104030192e56b
antoniquang <antoni.quang@gmail.com>
2021-11-10 00:54:15 +0700

    xx
    xx

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const io = require('socket.io')(server, {
 server.listen(process.env.PORT || 3000);
 
 
-const url = 'https://olymp168.com/api/';
-const token='b2x5bXAxNjhAZ21haWwuY29tOkAxMjNANDU2'
 var arr_chat=[];
 
 io.on('connection', function(socket) {
@@ -50,35 +48,6 @@ io.on('connection', function(socket) {
 
 
 
-
-//io.emit('notify_receive', {success:"true"});
-
-// setInterval(function(){
-//     console.log("Thanh toán");
-//         var data_api = { 
-//                     detect:"thanhtoantructiep",
-//                     type_manager:"check_status_all"
-//              }; 
-//         var url_api=url+"?detect=thanhtoantructiep&type_manager=check_status_all"      
-//         axios.post(url,data_api, {
-//             headers: {
-//                 'Authorization': 'Basic '+token,
-//             },
-//         }).then((res) => {
-//             console.log(res.data);
-//             list_customer=res.data.list_customer;
-//             if(list_customer!=null)
-//             {
-//                 list_customer.forEach(element => {
-//                     io.to(element.id_customer).emit('notify_receive', {success:"true",message:"Thanh toán thành công",amount:element.amount});
-//                 });
-//             }
-            
-//             //
-//         }).catch((error) => {
-
-//         })
-// },10000000)
 
 
 
